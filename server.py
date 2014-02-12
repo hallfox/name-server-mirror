1c6e64672b496faf70944d307882dfe1477b6a4c
Taylor Foxhall <lxmachina@gmail.com>
2014-02-11 22:26:49 -0500

    add maze game

--- server.py
+++ server.py
@@ -29,16 +29,53 @@ def index():
 @route('/', method='POST')
 def index():
 	name = request.forms.get('name')
-	request.session['name'] = name
+	request.session['user'] = {'name':name, 'solved_1':False, 'key':False}
 	return template('welcome', name=name)
 
 # Sample function using request.session dictionary
 @route('/hi')
 def hi():
-	name = request.session.get('name', None)
-	if name is not None:
+	user = request.session.get('user', None)
+	if user is not None:
+		name = user.get('name')
 		return 'logged in as {}'.format(name)
 	return 'not logged in'
+	
+@route('/maze')
+def maze():
+	user = request.session.get('user', None)
+	if user is not None:
+		name = user.get('name')
+		return template('maze', name=name)
+	return 'you must log in to play the maze game'
+
+@route('/maze', method='POST')
+def maze():
+	answer = request.forms.get('in')
+	if answer is not None:
+		if answer[:4] == "open":
+			request.session['user']['solved_1'] = True
+			return '<a href="/maze/lobby">The door opens easily. Good thing it was unlocked.</a>'
+		return template('game_over')
+
+@route('/maze/lobby')
+def lobby():
+	solved_p1 = request.session['user'].get('solved_1')
+	if solved_p1:
+		return template('lobby')
+	return "This story is told in a linear way. Don't try to get all timey-wimey with me."
+
+@route('/maze/lobby', method='POST')
+def lobby():
+	has_key = request.session['user'].get('key')
+	if has_key:
+		return template('end')
+	return template('game_over')
+	
+@route('/maze/key')
+def key():
+	request.session['user']['key'] = True
+	return template('key')
 
 # Start the server
 run(app, host='localhost', port=8080)
